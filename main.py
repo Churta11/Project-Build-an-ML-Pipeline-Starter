f335c159339a1bdb58684d5917a9745eaa1dbd2c
Churta11 <churta11@wgu.edu>
2025-12-12 20:16:31 -0600

    - Added mlflow run script for the test_regression_model step

--- main.py
+++ main.py
@@ -86,7 +86,7 @@ def go(config: DictConfig):
                 f"{config['main']['components_repository']}/train_val_test_split",
                 'main',
                 parameters = {
-                    'input': 'cleaned_sample.csv:latest',
+                    'input': 'cleaned_sample.csv    :latest',
                     'test_size': config['modeling']['test_size'],
                     'random_seed': config['modeling']['random_seed'],
                     'stratify_by': config['modeling']['stratify_by'],
@@ -100,22 +100,32 @@ def go(config: DictConfig):
             rf_config = os.path.abspath("rf_config.json")
             with open(rf_config, "w+") as fp:
                 json.dump(dict(config["modeling"]["random_forest"].items()), fp)  # DO NOT TOUCH
-
-            # NOTE: use the rf_config we just created as the rf_config parameter for the train_random_forest
-            # step
-
-            ##################
-            # Implement here #
-            ##################
+            _ = mlflow.run(
+                os.path.join(hydra.utils.get_original_cwd(), "src", "train_random_forest"),
+                "main",
+                env_manager="conda",
+                parameters={
+                    'trainval_artifact': 'trainval_data.csv:latest',
+                    'val_size': config['modeling']['val_size'],
+                    'random_seed': config['modeling']['random_seed'],
+                    'stratify_by': config['modeling']['stratify_by'],
+                    'max_tfidf_features': config['modeling']['max_tfidf_features'],
+                    'rf_config': rf_config,
+                    'output_artifact': 'random_forest_export',
+                },
+            )
 
             pass
 
         if "test_regression_model" in active_steps:
-
-            ##################
-            # Implement here #
-            ##################
-
+            _ = mlflow.run(
+                os.path.join(hydra.utils.get_original_cwd(), "components", "test_regression_model"),
+                'main',
+                parameters = {
+                    'mlflow_model': 'random_forest_export:prod',
+                    'test_dataset': "test_data.csv:latest"
+                 },
+            )
             pass
 
 
